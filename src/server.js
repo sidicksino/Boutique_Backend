d650190bec292c8f78492b11778e976cdb509486
sidicksino <sidickabdoulayesino1@gmail.com>
2025-08-17 19:04:23 +0200

    Add a route to update the profile image and upload in clodinary

--- src/server.js
+++ src/server.js
@@ -19,7 +19,8 @@ if (process.env.NODE_ENV === "production") job.start();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 
 // Simple route
