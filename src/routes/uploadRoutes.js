cb866f46d64606309538c01767cb7b2fbe1b8f60
sidicksino <sidickabdoulayesino1@gmail.com>
2025-08-17 18:23:41 +0200

    Add a route to update the profile image and upload in clodinary

--- src/routes/uploadRoutes.js
+++ src/routes/uploadRoutes.js
@@ -1,19 +1,27 @@
+const express = require('express');
+const cloudinary = require('../config/cloudinary');
+const { db } = require('../config/db');
+const authenticateToken = require('../middleware/auth');
+
+const router = express.Router(); // ⚡ il manquait ça
+
 router.post('/upload', authenticateToken, async (req, res) => {
   try {
-    console.log("Body received length:", req.body?.profile_photo?.length);
-
     const userId = req.user.user_id;
     const { profile_photo } = req.body;
+
     if (!profile_photo) {
       return res.status(400).json({ error: 'No file uploaded' });
     }
 
+    // upload sur Cloudinary
     const uploadResponse = await cloudinary.uploader.upload(profile_photo, {
       folder: "profile_photos",
     });
 
     const imageUrl = uploadResponse.secure_url;
 
+    // update en DB
     await db`
       UPDATE users 
       SET profile_photo = ${imageUrl}
@@ -26,3 +34,5 @@ router.post('/upload', authenticateToken, async (req, res) => {
     res.status(500).json({ error: 'Upload failed' });
   }
 });
+
+module.exports = router;
