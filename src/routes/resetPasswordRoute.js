3bb824031a95d0e46ed8fe5b174051985a6b6cd0
sidicksino <sidickabdoulayesino1@gmail.com>
2025-08-19 03:17:44 +0200

    Add route to changer password from profile

--- src/routes/resetPasswordRoute.js
+++ src/routes/resetPasswordRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const resetPasswordController = require('../controllers/resetPasswordController');
+const authenticateToken = require('../middleware/auth');
 
 router.post('/requestReset', resetPasswordController.requestReset);
 router.post('/verifyCode', resetPasswordController.verifyCode);
 router.post('/resetPassword', resetPasswordController.resetPassword);
-router.post('/changePassword', resetPasswordController.changePassword);
+router.post('/changePassword', authenticateToken, resetPasswordController.changePassword);
 
 module.exports = router;
